0d44e5cf5e45b39b086f65ab7c507f4ef56d1324
Aftab Farhan <aftabfarhan324@gmail.com>
2025-09-24 14:38:39 +0600

    toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import Navbar from "./Componend/NavBar/Navbar";
 import CounterBox from "./Componend/CounterBox/CounterBox";
 import Footer from "./Componend/Footer/Footer";
@@ -13,12 +13,14 @@ const assigmentData = async () => {
 };
 
 const assignment = assigmentData();
+
 const App = () => {
+  const [togle, setTogle] = useState("All");
   return (
     <div>
       <Navbar></Navbar>
       <CounterBox></CounterBox>
-      <CounterButton></CounterButton>
+      <CounterButton togle={togle} setTogle={setTogle}></CounterButton>
       <Suspense fallback={<Spinner></Spinner>}>
         <CardSections assignment={assignment}></CardSections>
       </Suspense>
--- src/Componend/CounterButton/CounterButton.jsx
+++ src/Componend/CounterButton/CounterButton.jsx
@@ -1,13 +1,40 @@
-import React from "react";
-
-const CounterButton = () => {
+const CounterButton = ({togle,setTogle}) => {
   return (
     <div className=" max-w-[1340px] mx-auto mt-15 flex justify-end gap-2">
       <div className=" bg-purple-100 rounded-lg text-md font-medium">
-          <button className="px-4 py-2 ">All</button>
-        <button className="px-4 py-2 ">Pending</button>
-        <button className="px-4 py-2 ">Submitted</button>
-        <button className="px-4 py-2 ">Reviewed</button>
+        <button
+          onClick={() => setTogle("All")}
+          className={`rounded-l-lg togle-btn ${
+            togle === "All" && "bg-purple-500 !text-white"
+          }`}
+        >
+          All
+        </button>
+
+        <button
+          onClick={() => setTogle("Pending")}
+          className={`togle-btn ${
+            togle === "Pending" && "bg-purple-500 !text-white"
+          }`}
+        >
+          Pending
+        </button>
+        <button
+          onClick={() => setTogle("Submitted")}
+          className={`togle-btn ${
+            togle === "Submitted" && "bg-purple-500 !text-white"
+          }`}
+        >
+          Submitted
+        </button>
+        <button
+          onClick={() => setTogle("Reviewed")}
+          className={`rounded-r-lg togle-btn ${
+            togle === "Reviewed" && "bg-purple-500 !text-white"
+          }`}
+        >
+          Reviewed
+        </button>
       </div>
     </div>
   );
